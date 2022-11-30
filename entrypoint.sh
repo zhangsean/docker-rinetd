86b27b8d6711da6959f40863f57797e9dd93ca26
Sean Zhang <zxf2342@qq.com>
2022-12-01 00:45:43 +0800

    参数3指定监听端口

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-echo "0.0.0.0 8000 $1 $2" > /etc/rinetd.conf
+echo "0.0.0.0 ${3:-8000} $1 $2" > /etc/rinetd.conf
 echo "logfile /var/log/rinetd.log" >> /etc/rinetd.conf
 /usr/sbin/rinetd -f -c /etc/rinetd.conf
