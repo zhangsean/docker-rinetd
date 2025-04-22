ee9e14431295ff838434d9ff466179c3f8a9ffe5
Sean Zhang <zxf2342@qq.com>
2025-04-22 12:07:42 +0800

    第4个参数可选udp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
-echo "0.0.0.0 ${3:-8000} $1 $2" > /etc/rinetd.conf
-echo "logfile /var/log/rinetd.log" >> /etc/rinetd.conf
+if [ ! -f /etc/rinetd.conf ]; then
+  proto=${4}
+  if [ $proto == udp ]; then
+    proto=/udp
+  fi
+  echo "0.0.0.0 ${3:-8000}$proto $1 $2$proto" > /etc/rinetd.conf
+  echo "logfile /var/log/rinetd.log" >> /etc/rinetd.conf
+fi
 /usr/sbin/rinetd -f -c /etc/rinetd.conf
